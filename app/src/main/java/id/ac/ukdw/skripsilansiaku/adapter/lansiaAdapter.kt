466fbb4532c42ad00e1a4a62561d05769e1eb9a9
Yoganicats <90562923+YoganicaTedjosantoso@users.noreply.github.com>
2024-06-08 23:08:22 +0700

    Delete

--- app/src/main/java/id/ac/ukdw/skripsilansiaku/adapter/lansiaAdapter.kt
+++
@@ -1,4 +0,0 @@
-package id.ac.ukdw.skripsilansiaku.adapter
-
-class lansiaAdapter {
-}
--- app/src/main/java/id/ac/ukdw/skripsilansiaku/dataclass/DataClassSpesialis.kt
+++
@@ -1,5 +0,0 @@
-package id.ac.ukdw.skripsilansiaku.dataclass
-
-class DataClassSpesialis (
-    var title: String,
-    var image: Int)
